bf3d5af78144434ff29abe91a6ae4d25cd6cde07
Thomas Billiet <billiet.thomas@outlook.com>
2024-03-05 15:26:12 +0100

    add not-found page

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -9,10 +9,13 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
   const outlet = useOutlet();
 
   return (
-    <div className="h-full bg-pampas dark:bg-gray-900" data-testid="main">
+    <div
+      className="flex h-full flex-col bg-pampas dark:bg-gray-900"
+      data-testid="main"
+    >
       <Navbar />
-      <div className="px-8 py-6 sm:px-4">
-        <div className="container mx-auto">
+      <div className="flex-1 px-8 py-6 sm:px-4">
+        <div className="container mx-auto h-full">
           {outlet ? <Outlet /> : children}
         </div>
       </div>
--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,7 +1,17 @@
-// TODO: Implement a not found page.
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return false;
+  return (
+    <main className="flex size-full flex-col items-center justify-center text-black dark:text-white">
+      <h1 className="-mt-12 text-7xl font-bold">Oops</h1>
+      <h2 className="mt-3 text-xl text-gray-800 dark:text-gray-300">
+        Page not found.
+      </h2>
+      <Link to="/" className="mt-3 text-xl font-medium underline">
+        Back to Home
+      </Link>
+    </main>
+  );
 };
 
 export default NotFound;
